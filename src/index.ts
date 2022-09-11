a23cf4b264c45d2204a015b56ceac912dfb19dd1
Sergi <sergi.jajanidze@gmail.com>
2022-09-11 12:38:17 +0400

    created slider class and initial methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('sdgsd');
+import { getTemplate } from "./utils/getTemplate";
+
+class Slider {
+  slider: HTMLElement | null;
+  state: string;
+  constructor(selector: string, state: string) {
+    this.slider = document.getElementById(selector);
+    this.state = state;
+    this.render(this.state);
+  }
+
+  private render(state: string) {
+    if (this.slider) {
+      this.slider.innerHTML = getTemplate(state);
+    }
+  }
+
+  moveHandler() {}
+
+  mouseUpHandler() {}
+
+  mouseDownHandler() {}
+}
+
+const slider = new Slider("slider", "");
---
+++ src/utils/getTemplate.ts
@@ -0,0 +1,8 @@
+export const getTemplate = (state: string) => {
+  return `
+        <div class="slider__before"></div>
+          <div class="slider__after">
+             <div class="slider__resize" data-type="resize"></div>
+        </div>
+    `;
+};
