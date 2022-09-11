72083cab65ea7b69f6beb65e1196e3d66a5e9b66
Sergi <sergi.jajanidze@gmail.com>
2022-09-11 13:47:30 +0400

    added eventhandlers/updating dom elemetns

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
+import { IState } from "./models/IState";
 import { getTemplate } from "./utils/getTemplate";
 
 class Slider {
   slider: HTMLElement | null;
-  state: string;
-  constructor(selector: string, state: string) {
+  state: IState;
+  currentClientX: number = 0;
+  constructor(selector: string, state: IState) {
     this.slider = document.getElementById(selector);
-    this.state = state;
+    this.state = {
+      ...state,
+      width: state.width || 375,
+    };
     this.render(this.state);
     this.listen();
   }
 
-  private render = (state: string) => {
+  private render = (state: IState) => {
     if (this.slider) {
       this.slider.innerHTML = getTemplate(state);
     }
+  };
+
+  private update = (props: IState) => {
+    this.state = {
+      ...this.state,
+      ...props,
+    };
+    this.render(this.state);
   };
 
   listen = () => {
@@ -21,24 +34,24 @@ class Slider {
     this.slider?.addEventListener("mouseup", this.mouseUpHandler);
   };
 
-  mouseDownHandler = (ev: Event) => {
-    console.log('downnadler');
-
+  mouseDownHandler = (ev: MouseEvent) => {
     if ((ev.target as HTMLElement)?.dataset.type === "resize") {
       this.slider?.addEventListener("mousemove", this.moveHandler);
     }
+
+    this.currentClientX = ev.clientX;
   };
 
   mouseUpHandler = () => {
-    console.log('uphnadler');
-
     this.slider?.removeEventListener("mousemove", this.moveHandler);
   };
 
-  moveHandler = () => {
-    console.log('movehnadler');
-    
+  moveHandler = (ev: MouseEvent) => {
+    const changedClientX = this.currentClientX - ev.clientX;
+
+    this.update({ width: this.state.width - changedClientX });
+    this.currentClientX = ev.clientX;
   };
 }
 
-const slider = new Slider("slider", "");
+const slider = new Slider("slider", {} as IState);
--- src/utils/getTemplate.ts
+++ src/utils/getTemplate.ts
@@ -1,7 +1,9 @@
-export const getTemplate = (state: string) => {
+import { IState } from "../models/IState";
+
+export const getTemplate = (state: IState) => {
   return `
         <div class="slider__before"></div>
-          <div class="slider__after">
+          <div class="slider__after" style="width: ${state.width}px">
              <div class="slider__resize" data-type="resize"></div>
         </div>
     `;
